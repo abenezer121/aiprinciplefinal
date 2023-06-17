85b5fd673f499a1fc5426fbacec66bac8c595635
abenezer121 <webgoat12@gmail.com>
2023-06-17 12:54:47 +0300

    fixing bfsanddfs

--- bfsanddfs/main.py
+++ bfsanddfs/main.py
@@ -1,23 +1,26 @@
 
   
-from graph import Graph
+
 from data import Data
-from node import Node   
-from problem import GraphProblem
-from dfs import dfs
-from bfs import bfs
+from bfs import BreadthFirstSearch
+from dfs import DepthFirstSearch
 
-visit_ethiopia= Graph(Data.returnmap(),False)
+map =  Data.returnmap()
 
 
+print("using breadth first search")
+# Call the function with an initial and a goal state
+print(BreadthFirstSearch.breadth_first_search(map, "Addis Ababa", "Awash"))
+print()
+print()
+
+print("using depth first search")
+print(DepthFirstSearch.dfs(map, "Addis Ababa", "Jimma"))
 
 
-visit_ethiopia_problem = GraphProblem('Addis Ababa','Shire', visit_ethiopia)
-finalnode = bfs.breadth_first_search(visit_ethiopia_problem)
-print("breadth_first_search of ", visit_ethiopia_problem.initial, " to ", visit_ethiopia_problem.goal, finalnode.solution())
 
 
 
-finalnode = dfs.depth_first_search(visit_ethiopia_problem)
- 
-print("depth_first_search of ", visit_ethiopia_problem.initial, " to ", visit_ethiopia_problem.goal, finalnode.solution())
+
+
+
